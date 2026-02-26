8d4063cda5b717d9a55eb21f8e6fc318a4921730
Víctor Julio Rodríguez Cerdas <46943476+vjulio@users.noreply.github.com>
2026-02-25 19:00:14 -0600

    primer push

---
+++ Vehiculo.API/Abstracciones/Modelos/Vehiculo.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Abstracciones.Modelos
+{
+    public class VehiculoBase
+    {
+        [Required(ErrorMessage = "La propiedad placa es requerida")]
+        [RegularExpression(@"[A-Za-z]{3}-[0-9]{3}")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "La propiedad color es requerida")]
+        public string Color { get; set; }
+
+        [Required(ErrorMessage = "La propiedad año es requerida")]
+        public int Anio { get; set; }
+
+        [Required(ErrorMessage = "La propiedad precio es requerida")]
+        public decimal Precio { get; set; }
+
+        [Required(ErrorMessage = "La propiedad correo es requerida")]
+        public string CorreoPropietario { get; set; }
+
+        [Required(ErrorMessage = "La propiedad teléfono es requerida")]
+        public string Telefono { get; set; }
+    }
+
+    public class VehiculoRequest : VehiculoBase
+    {
+        public Guid IdModelo { get; set; }
+    }
+
+    public class VehiculoResponse : VehiculoBase
+    {
+        public Guid IdModelo { get; set;}
+        public string Modelo   { get; set;}
+        public string Marca     { get; set;}
+
+
+    }
+}

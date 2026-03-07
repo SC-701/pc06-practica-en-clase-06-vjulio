f23338198d15be1a049aa9070bc4ba02d9d9013f
Víctor Julio Rodríguez Cerdas <46943476+vjulio@users.noreply.github.com>
2026-03-07 10:35:10 -0600

    UpdateFinal 1

--- Vehiculo.API/Abstracciones/Interfaces/Servicios/IRevisionServicio.cs
+++ Vehiculo.API/Abstracciones/Interfaces/Servicios/IRevisionServicio.cs
@@ -4,6 +4,6 @@ namespace Abstracciones.Interfaces.Servicios
 {
     public interface IRevisionServicio
     {
-        Task<Revision> Obtener(string placa);
+        Task<Revision?> Obtener(string placa);
     }
 }
